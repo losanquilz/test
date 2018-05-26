cf1b2b5de14ce7a2bc32b4e481bcfa1f1212e069
BrLi <rainman59118@gmail.com>
2018-05-26 18:57:11 +0800

    remove unused code

--- app.js
+++ app.js
@@ -10,18 +10,6 @@ var introduction = require('./routes/introduction');
 var mining = require('./routes/mining');
 var chart = require('./routes/chart');
 
-
-// var order = require('./routes/order');
-// var newOrder=require('./routes/newOrder');
-//var searchOrder = require('./routes/searchOrder');
-// var result = require('./routes/result');
-// var searchcustomer = require('./routes/searchCustomer');
-// var customer = require('./routes/customer');
-// var inventory = require('./routes/inventory');
-// var MongoStore = require('connect-mongo')(connect)
-// var settings = require('./settings');
-
-
 var app = express();
 
 // view engine setup
@@ -105,46 +93,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-// $(document).ready(function(){
-// 	$.ajax({
-// 		url: "http://localhost/chartjs/data.php",
-// 		method: "GET",
-// 		success: function(data) {
-// 			console.log(data);
-// 			var player = [];
-// 			var score = [];
-//
-// 			for(var i in data) {
-// 				player.push("Player " + data[i].playerid);
-// 				score.push(data[i].score);
-// 			}
-//
-// 			var chartdata = {
-// 				labels: player,
-// 				datasets : [
-// 					{
-// 						label: 'Player Score',
-// 						backgroundColor: 'rgba(200, 200, 200, 0.75)',
-//             borderColor: 'rgba(200, 200, 200, 0.75)',
-// 						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-// 						hoverBorderColor: 'rgba(200, 200, 200, 1)',
-// 						data: score
-// 					}
-// 				]
-// 			};
-//
-// 			var ctx = $("#mycanvas");
-//
-// 			var barGraph = new Chart(ctx, {
-// 				type: 'bar',
-// 				data: chartdata
-// 			});
-// 		},
-// 		error: function(data) {
-// 			console.log(data);
-// 		}
-// 	});
-// });
+
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,8 @@
 var express = require('express');
 var router = express.Router();
-// var order = require('../models/order');
-// var Order_Product = require('../models/Order_Product');
-// var RFM=require('../models/RFM');
 
-
-    router.get('/', function(req, res, next) {
-        // RFM.get(function(err,RFM_list) {
-            // if(err) {
-                // res.status = err.code;
-                // res.json(err);
-            // } else {
-                // for(i=0;i<RFM_list.length;i++){
-                    // var RFM_edit=new RFM({
-                        // RFM_Score : RFM_list[i].RFM_Score,
-                        // BEI : RFM_list[i].BEI
-                    // });
-
-                    // RFM_edit.save(function(err) {
-                        // if(err) {
-                            // res.status = err.code;
-                            // res.json(err);
-                        // } else {
-                            // console.log('BEI success');
-                        // }
-                    // });
-                // }
-
-            // }
-        // });
-
-        // Order_Product.countAmount(1,function(err, list){
-            // if(err){
-                // res.status(err.code);
-                // res.json(err);
-            // }else{
-                // console.log(list);
-                // for(i=0;i<list.length;i++){
-                    // console.log(list[i].Order_ID);
-                    // var editOrderForm=new order({
-                        // id : list[i].Order_ID,
-                        // O_amount : list[i].amount
-                    // });
-                    // console.log(editOrderForm.id);//testline
-
-                    // editOrderForm.save(function(err) {
-                        // if(err) {
-                            // res.status = err.code;
-                            // res.json(err);
-                        // } else {
-                            // console.log('success');
-                        // }
-                    // });
-                // }
-            // }
-        // })
+router.get('/', function(req, res, next) {
     res.render('index', { title: 'Index' });
-    });
+});
 
 module.exports = router;
